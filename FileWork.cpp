320eab020936450adf9292c2b364f66ccc753495
malou-c <ksh1957nr@mail.ru>
2020-03-11 13:05:15 +0700

    Chenged FileWork add draw_head

--- FileWork.cpp
+++ FileWork.cpp
@@ -1,4 +1,4 @@
-#pragma once
+
 #include "FileWork.h"
 
 using namespace std;
@@ -9,6 +9,64 @@ void FileWork::add_msg(string msg, string path)
     fs.open(path, fs.out | fs.app);
     if (fs.is_open()) {
         cout << "file open" << endl;
+        fs << msg;
+
+    } else {
+        cout << "error file" << endl;
+    }
+    fs.close();
+}
+void FileWork::draw_head(string path)
+{
+    string msg = R"( 
+ <!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>chessviz</title>
+  <style type="text/css">
+    table.chessboard {
+      border: 5px solid #333;
+      border-collapse: collapse;
+      height: 320px;
+      margin: 20px;
+      width: 320px;
+    }
+    table.chessboard caption {
+      text-align: left;
+    }
+    table.chessboard td {
+      background-color: #fff;
+      font-size: 25px;
+      height: 40px;
+      text-align: center;
+      vertical-align: middle;
+      width: 40px;
+    }
+    table.chessboard tr:nth-child(odd) td:nth-child(even),
+    table.chessboard tr:nth-child(even) td:nth-child(odd) {
+      background-color: #999;
+    }
+
+    table.chessboard .white.king:before   { content: "\2654"; }
+    table.chessboard .white.queen:before  { content: "\2655"; }
+    table.chessboard .white.rook:before   { content: "\2656"; }
+    table.chessboard .white.bishop:before { content: "\2657"; }
+    table.chessboard .white.knight:before { content: "\2658"; }
+    table.chessboard .white.pawn:before   { content: "\2659"; }
+
+    table.chessboard .black.king:before   { content: "\265A"; }
+    table.chessboard .black.queen:before  { content: "\265B"; }
+    table.chessboard .black.rook:before   { content: "\265C"; }
+    table.chessboard .black.bishop:before { content: "\265D"; }
+    table.chessboard .black.knight:before { content: "\265E"; }
+    table.chessboard .black.pawn:before   { content: "\265F"; }
+  </style>
+</head>)";
+    fs.open(path, fs.out | fs.app);
+    if (fs.is_open()) {
+        cout << "File open" << endl;
+
         fs << msg;
 
     } else {
