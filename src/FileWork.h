db666ea1c26d918ae38c0e899b4b8229fd86da39
malou-c <ksh1957nr@mail.ru>
2020-05-07 23:12:17 +0700

    Add a lot of funk
    добавлены  функции вывода которые  рисуют в  html file
    они однотипные и  не сложные

--- src/FileWork.h
+++ src/FileWork.h
@@ -6,5 +6,10 @@
 class FileWork {
 public:
     void add_msg(std::string msg, std::string path);
-    void draw_head(std::string path);
+    void add_head(std::string path);
+    void add_step(std::string step, std::string path);
+    void add_figure(char figure, std::string path);
+    void add_end(std::string path);
+    void add_complete(std::string path);
+    void add_error(std::string step, std::string path);
 };
